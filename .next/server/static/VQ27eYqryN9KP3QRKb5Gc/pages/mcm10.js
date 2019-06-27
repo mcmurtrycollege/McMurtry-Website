b1231da41e00553e4fa877674d4ba29a88197a20
Nick Meisburger <ncm5@rice.edu>
2019-06-27 14:06:13 -0500

    bug fix

---
+++ .next/server/static/VQ27eYqryN9KP3QRKb5Gc/pages/mcm10.js
@@ -0,0 +1,279 @@
+module.exports =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = require('../../../ssr-module-cache.js');
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		var threw = true;
+/******/ 		try {
+/******/ 			modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 			threw = false;
+/******/ 		} finally {
+/******/ 			if(threw) delete installedModules[moduleId];
+/******/ 		}
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 24);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "+7wB":
+/***/ (function(module, exports) {
+
+
+
+/***/ }),
+
+/***/ 24:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__("qHBd");
+
+
+/***/ }),
+
+/***/ "9Bhh":
+/***/ (function(module, exports) {
+
+module.exports = require("rebass");
+
+/***/ }),
+
+/***/ "BEZE":
+/***/ (function(module, exports) {
+
+
+
+/***/ }),
+
+/***/ "L5GA":
+/***/ (function(module, exports) {
+
+
+
+/***/ }),
+
+/***/ "ZHh6":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("cDcd");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__("xnum");
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_head__WEBPACK_IMPORTED_MODULE_1__);
+
+
+
+var Header = function Header() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(next_head__WEBPACK_IMPORTED_MODULE_1___default.a, null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("meta", {
+    charSet: "utf-8"
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("link", {
+    rel: "shortcut icon",
+    type: "image/x-icon",
+    href: "../static/favicon.ico"
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("link", {
+    href: "https://fonts.googleapis.com/css?family=Montserrat",
+    rel: "stylesheet"
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("meta", {
+    name: "viewport",
+    content: "width=device-width, initial-scale=1, shrink-to-fit=no"
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("meta", {
+    name: "theme-color",
+    content: "#000000"
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("title", null, "McMurtry College"));
+};
+
+/* harmony default export */ __webpack_exports__["a"] = (Header);
+
+/***/ }),
+
+/***/ "cDcd":
+/***/ (function(module, exports) {
+
+module.exports = require("react");
+
+/***/ }),
+
+/***/ "qHBd":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+
+// EXTERNAL MODULE: external "react"
+var external_react_ = __webpack_require__("cDcd");
+var external_react_default = /*#__PURE__*/__webpack_require__.n(external_react_);
+
+// EXTERNAL MODULE: ./components/header.js
+var header = __webpack_require__("ZHh6");
+
+// EXTERNAL MODULE: external "react-parallax"
+var external_react_parallax_ = __webpack_require__("qr8A");
+
+// EXTERNAL MODULE: external "rebass"
+var external_rebass_ = __webpack_require__("9Bhh");
+
+// EXTERNAL MODULE: ./components/10/homepage/homepage.css
+var homepage = __webpack_require__("BEZE");
+
+// EXTERNAL MODULE: ./components/10/flip/flip.css
+var flip = __webpack_require__("+7wB");
+
+// CONCATENATED MODULE: ./components/10/flip/flip.js
+
+
+
+
+var flip_FlipCard = function FlipCard(props) {
+  return external_react_default.a.createElement("div", {
+    className: "flipcard"
+  }, external_react_default.a.createElement("div", {
+    className: "front"
+  }, external_react_default.a.createElement(external_rebass_["Image"], {
+    src: props.img,
+    alt: ""
+  })), external_react_default.a.createElement("div", {
+    className: "back"
+  }, external_react_default.a.createElement("p", null, "this is the back")));
+};
+
+/* harmony default export */ var flip_flip = (flip_FlipCard);
+// CONCATENATED MODULE: ./components/10/homepage/homepage.js
+
+
+
+
+
+
+var homepage_MCM10 = function MCM10() {
+  return external_react_default.a.createElement("div", null, external_react_default.a.createElement(external_react_parallax_["Parallax"], {
+    bgImage: "../../../static/mcm10bg.svg",
+    bgImageAlt: "",
+    strength: 800
+  }, external_react_default.a.createElement("div", {
+    className: "parallax-wrapper1"
+  }, external_react_default.a.createElement("div", {
+    className: "mcm10-title-container"
+  }, external_react_default.a.createElement("div", {
+    className: "mcm10-title"
+  }, external_react_default.a.createElement(external_rebass_["Image"], {
+    src: "../../../static/mcm10.svg",
+    alt: "MCM10"
+  }))), external_react_default.a.createElement("div", {
+    className: "mcm10-btn"
+  }, external_react_default.a.createElement("p", null, "Get Tickets")), external_react_default.a.createElement("div", {
+    className: "mcm10-btn"
+  }, external_react_default.a.createElement("p", null, "Become a Sponsor")))), external_react_default.a.createElement("div", {
+    className: "parallax-wrapper2"
+  }, external_react_default.a.createElement("h1", null, "MCM10")));
+};
+
+/* harmony default export */ var homepage_homepage = (homepage_MCM10);
+// EXTERNAL MODULE: ./pages/page.css
+var page = __webpack_require__("L5GA");
+
+// CONCATENATED MODULE: ./pages/mcm10/index.js
+
+
+
+
+
+var mcm10_MCM10 = function MCM10() {
+  return external_react_default.a.createElement("div", null, external_react_default.a.createElement(header["a" /* default */], null), external_react_default.a.createElement(homepage_homepage, null));
+};
+
+/* harmony default export */ var mcm10 = __webpack_exports__["default"] = (mcm10_MCM10);
+
+/***/ }),
+
+/***/ "qr8A":
+/***/ (function(module, exports) {
+
+module.exports = require("react-parallax");
+
+/***/ }),
+
+/***/ "xnum":
+/***/ (function(module, exports) {
+
+module.exports = require("next/head");
+
+/***/ })
+
+/******/ });

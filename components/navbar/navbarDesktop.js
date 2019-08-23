3e402ada547315166cc1e54cad9fddf3e7b47e98
unknown <ncm5@rice.edu>
2019-08-23 13:34:42 -0700

    unbreak safari

--- components/navbar/navbarDesktop.js
+++ components/navbar/navbarDesktop.js
@@ -38,7 +38,7 @@ class NavbarDesktop extends React.Component {
             <div className='navbar-wrapper' style={style}>
                 <Link href='/'>
                     <div className='header-crest'>
-                        <Image src='../../static/logo.svg' alt="" />
+                        {/* <Image src='../../static/logo.svg' alt="" /> */}
                         <p>McMurtry College</p>
                     </div>
                 </Link>

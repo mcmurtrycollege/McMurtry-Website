55e4159359593faf49ec55567d697f4428714486
nmeisburger0 <ncm5@rice.edu>
2019-08-27 15:01:55 -0500

    stuff

--- docs/_next/static/runtime/webpack.js
+++ docs/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c9dc2123849822aefd32";
+/******/ 	var hotCurrentHash = "f3c560b28b0a04ab15a7";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;

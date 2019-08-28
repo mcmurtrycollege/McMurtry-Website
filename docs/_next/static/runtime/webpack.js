b655838ff96eefb87bdd6e5e5775064d4ce788a5
nmeisburger0 <ncm5@rice.edu>
2019-08-27 21:07:36 -0500

    added associates

--- docs/_next/static/runtime/webpack.js
+++ docs/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f3c560b28b0a04ab15a7";
+/******/ 	var hotCurrentHash = "d8ea13313daa91218e97";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;

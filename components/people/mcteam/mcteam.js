011ae9c7e1b8a1a75ac1878fb94aca9152291f0f
shaqque <shaqqywyan@gmail.com>
2020-05-22 19:03:20 -0700

    add jackie's image

--- components/people/mcteam/mcteam.js
+++ components/people/mcteam/mcteam.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Flex } from 'rebass';
+import { Box, Flex, Image } from 'rebass';
 import './mcteam.css';
 import Title from '../../general/title';
 import { mcteam_positions } from './mcteam.json';
@@ -41,13 +41,16 @@ export default class McTeam extends React.Component {
                 </div>
                 <Box width={[1, 0.9, 0.8, 0.7]} ml='auto' mr='auto' mb={50} className='position-content'>
                     <h2 className='mcteam-name'>{mcteam_positions[this.state.current].names}</h2>
+                    <Box width={[0.8, 0.6]} mx='auto' >
+                        <Image src={mcteam_positions[this.state.current].image} />
+                    </Box>
                     {
                         mcteam_positions[this.state.current].body.map(line => (
                             <p key={`L-${mcteam_positions[this.state.current].body.indexOf(line)}`}>{line}</p>
                         ))
                     }
                     {
-                        this.state.current === 5 ? (
+                        this.state.current === 4 ? (
                             <ul>
                                 <li>Buy stamps and get envelopes</li>
                                 <li>Pick up mail and packages</li>

33e48939de7223e73b53278cc81fda24c31cc424
Tate Ward <tategward@outlook.com>
2020-06-03 00:39:12 -0500

    deploy site

--- docs/_next/static/runtime/webpack.js
+++ docs/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9b3cf33d19973b1bf2cf";
+/******/ 	var hotCurrentHash = "f21fbff6378edfad2586";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;

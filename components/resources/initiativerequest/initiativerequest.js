d4416b8602cf3165df6646a8c3772914053ee857
shaqque <shaqqywyan@gmail.com>
2020-08-09 11:05:04 -0700

    update initiatives request form

--- components/resources/initiativerequest/initiativerequest.js
+++ components/resources/initiativerequest/initiativerequest.js
@@ -7,10 +7,10 @@ const InitiativeRequest = () => (
     <div className='initiative-request-page'>
         <Title width={340} title="Initiative Request" />
         <div className='wide-initiative-request-form'>
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfWw9BThDp7wDgxtYYvwRomD-myrMXZYP6ve-TzincU81ViBg/viewform?embedded=true" width="700" height="2000" frameBorder="0" scrolling="yes" type="text/javascript" title="Initiatives Request"></iframe>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScdRhMUepjl6MwMTGBz2s9Q8fUrXqmC_v--q8qxr4PBdMwF8Q/viewform?embedded=true" width="700" height="2000" frameBorder="0" scrolling="yes" type="text/javascript" title="Initiatives Request"></iframe>
         </div>
         <div className='narrow-initiative-request-form'>
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfWw9BThDp7wDgxtYYvwRomD-myrMXZYP6ve-TzincU81ViBg/viewform?embedded=true" width="360" height="2000" frameBorder="0" scrolling="yes" type="text/javascript" title="Initiatives Request"></iframe>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScdRhMUepjl6MwMTGBz2s9Q8fUrXqmC_v--q8qxr4PBdMwF8Q/viewform?embedded=true" width="360" height="2000" frameBorder="0" scrolling="yes" type="text/javascript" title="Initiatives Request"></iframe>
         </div>
     </div>
 )

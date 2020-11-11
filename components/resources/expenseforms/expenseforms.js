0730839119147b72c669d2898f3cabbc7d1ec98e
shaqque <shaqqywyan@gmail.com>
2020-11-10 18:46:30 -0800

    use iframe instead of embed tag for expense forms

--- components/resources/expenseforms/expenseforms.js
+++ components/resources/expenseforms/expenseforms.js
@@ -77,7 +77,7 @@ class ExpenseForms extends React.Component {
                     }
                 </Flex>
                 <div className='embedded-form'>
-                    <embed src={forms[this.state.slide].document} type="application/pdf" width="600" height="820px" page="7" scrollbar="1" toolbar="1" />
+                    <iframe src={forms[this.state.slide].document} type="application/pdf" width="600" height="820px" page="7" scrollbar="1" toolbar="1" />
                 </div>
                 <div className='form-link-container'>
                     <a href={forms[this.state.slide].document} target='_blank' rel="noopener noreferrer" style={{ textDecoration: 'none' }}>

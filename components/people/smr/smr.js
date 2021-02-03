ccdd0969f272695cdf5145d71586f8b56d1a6882
Tate Ward <tategward@outlook.com>
2021-02-03 00:25:38 -0600

    add smr info

--- components/people/smr/smr.js
+++ components/people/smr/smr.js
@@ -1,13 +1,15 @@
 import Title from '../../general/title';
 import { Box } from 'rebass';
-import { smr_bio } from './smr.json';
+import Cards from '../../general/contactcards';
+import { smr } from './smr.json';
 import './smr.css';
 
 const SMR = () => (
     <div className='smr-page'>
         <Title title='Student Maintenance Rep' width={350} smallTest={true} />
         <Box width={[0.8, 0.5]} ml='auto' mr='auto'>
-            <p>{smr_bio}</p>
+            <h2 className = 'smr-name'>Krishna Chandra</h2>
+            <h4 className = 'smr-email'>Email: kc60@rice.edu</h4>
         </Box>
     </div>
 )

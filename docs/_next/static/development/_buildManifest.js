6cd731f28e872fca0efaddb60bd226b7e98883d2
Tate Ward <tategward@outlook.com>
2021-03-12 22:05:03 -0600

    deploy site

--- docs/_next/static/development/_buildManifest.js
+++ docs/_next/static/development/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = (function(a,b){return {"/resources/BlackLivesMatter":[a,b],"/resources/diversityResources":[a,b]}}("static\u002Fcss\u002Fstyles.chunk.css","static\u002Fchunks\u002Fstyles.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/index":[a,b],"/people/associates":[a,b]}}("static\u002Fcss\u002Fstyles.chunk.css","static\u002Fchunks\u002Fstyles.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()

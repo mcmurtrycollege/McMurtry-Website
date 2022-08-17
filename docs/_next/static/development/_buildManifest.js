41f7b966fbedea13135cfe7fe9982fc3e82889ae
Tate Ward <tategward@outlook.com>
2022-08-17 16:08:43 -0500

    deploy site

--- docs/_next/static/development/_buildManifest.js
+++ docs/_next/static/development/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = (function(a,b){return {"/oweek":[a,b],"/oweek/mcmtour":[a,b],"/oweek/oweekgroups":[a,b],"/oweek/yourroom":[a,b]}}("static\u002Fcss\u002Fstyles.chunk.css","static\u002Fchunks\u002Fstyles.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {"/people/committees":["static\u002Fcss\u002Fstyles.chunk.css","static\u002Fchunks\u002Fstyles.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()

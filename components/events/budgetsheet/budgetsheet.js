11a02f8af0fdaf8dc4ffec99a33eaa33ea6e894f
Tate Ward <tategward@outlook.com>
2022-12-16 20:07:11 -0600

    update spring 2023 budget sheet and expenditure form

--- components/events/budgetsheet/budgetsheet.js
+++ components/events/budgetsheet/budgetsheet.js
@@ -7,9 +7,9 @@ export default class BudgetSheet extends React.Component {
     render() {
         return (
             <div className='budget-sheet-page'>
-                <Title width={400} title="Fall Budget Sheet" />
-                <Box width={[0.7, 0.21]} ml='auto' mr='auto'>
-                    <p><a href="https://docs.google.com/spreadsheets/d/1AaKZA4lr3F9hOZFXsOOjkV898pSJUsHbZkT-lkaQZrI/edit?usp=sharing">Link to the McMurtry Fall 2022 Budget Sheet</a></p>
+                <Title width={400} title="Spring Budget Sheet" />
+                <Box width={[0.8, 0.25]} ml='auto' mr='auto'>
+                    <p><a href="https://docs.google.com/spreadsheets/d/1fms3QPytS6SwAQ6_BN9YRVsvFbd_UiKRLAbQk-QfGSg/edit?usp=sharing">Link to the McMurtry Spring 2023 Budget Sheet</a></p>
                 </Box>
             </div>
         )

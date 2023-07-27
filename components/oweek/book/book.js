18547fb36f73db0f71d870f3a85f17e8992cb706
Emily Huang <71594652+emilyhtx14@users.noreply.github.com>
2023-07-26 20:29:07 -0400

    changed headshots and updated oweek book

--- components/oweek/book/book.js
+++ components/oweek/book/book.js
@@ -4,9 +4,9 @@ const Book = () => (
     <div className='oweek-page'>
         <h1 className='oweek-title'>The O-Week Book</h1>
         <div className='oweek-book'>
-            <embed src='../../../static/oweek/oweekbook.pdf' type="application/pdf" width="600" height="820px" page="7" scrollbar="1" toolbar="1" />
+            <embed src='../../../static/oweek/oweekbook-2023.pdf' type="application/pdf" width="600" height="820px" page="7" scrollbar="1" toolbar="1" />
         </div>
-        <a href='../../../static/oweek/oweekbook.pdf' target='_blank' rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
+        <a href='../../../static/oweek/oweekbook-2023.pdf' target='_blank' rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
             <div className='oweek-book-download'>
                 <img src="https://icon.now.sh/file_download/DCDFE5/60" alt="" />
                 <p>The O-Week Book</p>

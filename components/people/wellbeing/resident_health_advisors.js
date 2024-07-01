7eb817d7f22edd1b4694488653e7e2e35101b928
chu-andrew <chu.andrew.h@gmail.com>
2024-06-30 23:13:24 -0700

    refactor expandable cards

--- components/people/wellbeing/resident_health_advisors.js
+++ components/people/wellbeing/resident_health_advisors.js
@@ -1,56 +1,16 @@
 import React from 'react';
-import { Box, Flex, Image } from 'rebass';
+import { Box, Flex } from 'rebass';
 import Title from '../../general/title';
-import ContactCards from '../../general/contactcards';
+import ExpandableCard from '../../general/expandablecards';
 import './wellbeing.css';
 import { head_RHAs, RHAs } from './wellbeing.json';
 
-class RhaCard extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = { contentVisible: false }
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick() {
-        this.setState({ contentVisible: !this.state.contentVisible })
-    }
-
-    render() {
-        let dropdownStyle = {}
-        let dropdownImageStyle = {}
-        dropdownStyle.maxHeight = this.state.contentVisible ? ('650px') : ('0')
-        dropdownImageStyle.maxHeight = this.state.contentVisible ? ('0') : ('450px')
-
-        return (
-            <Box width={1} onClick={this.handleClick}>
-                <div className={this.state.contentVisible ? 'rha-card-visible' : 'rha-card-hidden'}>
-                    <div className='rha-image-dropdown' style={dropdownImageStyle}>
-                        <Box className='rha-image' width={[0.9, 0.7]}>
-                            <Image src={this.props.photo} alt='' />
-                        </Box>
-                    </div>
-                    <h2 className='name'>{this.props.name}</h2>
-
-                    <h3 className="contact-phone"><img src="../../../../static/icons/phone.svg" alt="Phone: " />{' '}{this.props.phone}</h3>
-                    <a href={`mailto:${this.props.email}`} className='link'>
-                        <h3 className={this.state.contentVisible ? 'contact-email-visible' : 'contact-email-hidden'}><img src="../../../../static/icons/email.svg" alt="Email: " />{' '}{this.props.email}</h3>
-                    </a>
-
-                    <div style={dropdownStyle} className='rha-dropdown'>
-                        <p><strong>Bio: </strong>{this.props.bio}</p>
-                    </div>
-                </div>
-            </Box>
-        )
-    }
-}
 
 const ResidentHealthAdvisors = () => {
     let head_entries = [];
     for (let i = 0; i < head_RHAs.members.length; i++) {
         head_entries.push(
-            <RhaCard key={`${head_RHAs.members[i].name}`} name={head_RHAs.members[i].name}
+            <ExpandableCard key={`${head_RHAs.members[i].name}`} name={head_RHAs.members[i].name}
                 bio={head_RHAs.members[i].bio} photo={head_RHAs.members[i].photo} email={head_RHAs.members[i].email}
                 phone={head_RHAs.members[i].phone} />
         )
@@ -58,48 +18,48 @@ const ResidentHealthAdvisors = () => {
     let entries = [];
     for (let i = 0; i < RHAs.members.length; i++) {
         entries.push(
-            <RhaCard key={`${RHAs.members[i].name}`} name={RHAs.members[i].name}
+            <ExpandableCard key={`${RHAs.members[i].name}`} name={RHAs.members[i].name}
                 bio={RHAs.members[i].bio} photo={RHAs.members[i].photo} email={RHAs.members[i].email}
                 phone={RHAs.members[i].phone} />
         )
     }
     return (
-    <div className='wellbeing-page'>
-        <Title width={300} title="Rice Health Advisors" smallText={true} />
-        <Box width={[1, 0.7, 0.6, 0.5]} ml='auto' mr='auto' className='wellbeing-description'>
-            {RHAs.description}
-            <p align="center"><strong>Click to view the RHAs' bios!</strong></p>
-        </Box>
+        <div className='wellbeing-page'>
+            <Title width={300} title="Rice Health Advisors" smallText={true} />
+            <Box width={[1, 0.7, 0.6, 0.5]} ml='auto' mr='auto' className='wellbeing-description'>
+                {RHAs.description}
+                <p align="center"><strong>Click to view the RHAs' bios!</strong></p>
+            </Box>
 
-        <Box width={[330]} ml='auto' mr='auto'>
-            <h1 className='division-title'>Head RHAs</h1>
-        </Box>
-        <Box width={[1, 1, 0.9, 0.8]} ml='auto' mr='auto'>
-            <Flex flexWrap='wrap' flexDirection='row' justifyContent='center'>
-                {
-                    head_entries.map(entry => (
-                        <Flex flexDirection='column' width={[1, 0.33]} key={`C+${head_entries.indexOf(entry)}`}>
-                            {entry}
-                        </Flex>
-                    ))
-                }
-            </Flex>
-        </Box>
-        <Box width={[330]} ml='auto' mr='auto'>
-            <h1 className='division-title'>RHAs</h1>
-        </Box>
-        <Box width={[1, 1, 0.9, 0.8]} ml='auto' mr='auto'>
-            <Flex flexWrap='wrap' flexDirection='row' justifyContent='center'>
-                {
-                    entries.map(entry => (
-                        <Flex flexDirection='column' width={[1, 0.33]} key={`C+${entries.indexOf(entry)}`}>
-                            {entry}
-                        </Flex>
-                    ))
-                }
-            </Flex>
-        </Box>
-    </div>
+            <Box width={[330]} ml='auto' mr='auto'>
+                <h1 className='division-title'>Head RHAs</h1>
+            </Box>
+            <Box width={[1, 1, 0.9, 0.8]} ml='auto' mr='auto'>
+                <Flex flexWrap='wrap' flexDirection='row' justifyContent='center'>
+                    {
+                        head_entries.map(entry => (
+                            <Flex flexDirection='column' width={[1, 0.33]} key={`C+${head_entries.indexOf(entry)}`}>
+                                {entry}
+                            </Flex>
+                        ))
+                    }
+                </Flex>
+            </Box>
+            <Box width={[330]} ml='auto' mr='auto'>
+                <h1 className='division-title'>RHAs</h1>
+            </Box>
+            <Box width={[1, 1, 0.9, 0.8]} ml='auto' mr='auto'>
+                <Flex flexWrap='wrap' flexDirection='row' justifyContent='center'>
+                    {
+                        entries.map(entry => (
+                            <Flex flexDirection='column' width={[1, 0.33]} key={`C+${entries.indexOf(entry)}`}>
+                                {entry}
+                            </Flex>
+                        ))
+                    }
+                </Flex>
+            </Box>
+        </div>
     )
 }
 

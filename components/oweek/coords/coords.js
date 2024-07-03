d55be7327531b9f70539cba617b733264a24915a
fnemati <fn11@rice.edu>
2024-07-03 18:52:39 -0500

    oweek updates

--- components/oweek/coords/coords.js
+++ components/oweek/coords/coords.js
@@ -52,7 +52,16 @@ const Coords = () => (
             <h2 className='oweek-subtitle'>Gale Gai</h2>
         </div>
         <p>
-            COMING SOON!
+            Congratulations, you’ve made it to Rice! Double congratulations, you are joining McMurtry College, the BEST residential college on campus! 
+        </p>
+        <p>
+            My name is Gale, and I call Shenzhen, China, my home. I left home for the US when I was fourteen, and spent my high school years in Massachusetts before Rice. Here, I study Statistics and Managerial Economics, with the hope of using them for impactful work in the future! Personally, I’ve struggled to find the right major for myself, so it is totally okay if you are coming in not knowing exactly what you want to study.  
+        </p>
+        <p>
+            At Rice, I also sing in the Rice Apollos, the all-male acapella group on campus! I also helped found the Rice Chamber Music Club, where I play the violin and get campus musicians together to work some musical magic. At Murt, I am also your Prime Minister, and I oversee committees that plan lots of fun events! In my spare time, I watch a lot of sports and movies, play video games (recently acquired a PS5), and love a good game of poker. You will soon find your own niche and passions at Rice, too!
+        </p>
+        <p>
+            I am so glad to be your O-Week Coordinator this year alongside Francesca and Sean! We have delicately and intentionally crafted a fun, fulfilling, and meaningful week ahead of you, making sure of a smooth transition for you into college. So, get excited to meet your advisors and other new Murts, and I can’t wait to see y’all!
         </p>
         
         <div>
@@ -98,7 +107,19 @@ const Coords = () => (
         <div>
             <h2 className='oweek-subtitle'>Sean Nyangeri</h2>
             <p>
-                COMING SOON!
+                My name is Sean and I am a junior majoring in Sociology and Health Sciences, minoring in African & African American Studies. I was born and raised in Houston (h-townnn babyyyy), so I know the area around Rice and beyond quite well and I’d be super willing to give you any recs for things to do, places to eat at, etc.
+            </p>
+            <p>
+                Academics-wise, I love learning about how society and the world around me is organized and Blackness in different contexts. I also am really passionate about the social dimensions of health and societal inequalities. Besides that, I’m interested in history, anthropology, and foreign languages & cultures (fun fact: I can speak 2 languages fluently, English & Spanish, and I’m learning/can get around in 2 others, French & Yoruba).
+            </p>
+            <p>
+                On campus, I currently serve as the Director of Community for the Rice African Student Association (RASA), as well as being in the Black Student Association and being a member of the RASA Dance team. I am one of the head RHAs for McMurtry this upcoming year, so you’ll definitely see me around Murt here and there. Additionally, I am involved with the Center for Civic Leadership and DEI here on campus. All that is to say that I am also here if you everrrrr have any questions about ways to get involved on campus.
+            </p>
+            <p>
+                In my free time, I am such a hugeee foodieee as well as spending time with friends, going to museums, watching TV and movies. I also enjoy keeping up with soccer (or football as known by true fans hehe), specifically Premier League and La Liga (loveee Arsenal and FC Barcelona). Lastly, I adoreeee traveling and exploring new places, ask me about my study abroad experiences in Spain, France, Italy, and Costa Rica :)))))
+            </p>
+            <p>
+                I hope you’ve enjoyed reading my cute little bio and I am beyond thrilled to meet you all in August!!!
             </p>
         </div>
     </div>

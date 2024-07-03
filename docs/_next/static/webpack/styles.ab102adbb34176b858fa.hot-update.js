ec17cbb88579c413f727d697fbf01eaa9696bb93
aasumar123 <aas24@rice.edu>
2024-07-03 02:06:47 -0500

    Revert the last commit

---
+++ docs/_next/static/webpack/styles.ab102adbb34176b858fa.hot-update.js
@@ -0,0 +1,33 @@
+webpackHotUpdate_N_E("styles",{
+
+/***/ "./components/general/footer/footer.css":
+false,
+
+/***/ "./components/navbar/NavbarDesktop.css":
+false,
+
+/***/ "./components/navbar/navbarMobile.css":
+false,
+
+/***/ "./components/oweek/groups/groups.css":
+false,
+
+/***/ "./components/splashpage/intropage.css":
+false,
+
+/***/ 1:
+false,
+
+/***/ 2:
+false,
+
+/***/ 3:
+false,
+
+/***/ 4:
+false,
+
+/***/ 8:
+false
+
+})

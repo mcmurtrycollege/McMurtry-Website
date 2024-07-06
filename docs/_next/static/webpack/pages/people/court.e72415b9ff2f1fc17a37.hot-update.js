e76a3e3d61ed60c66d6dcd5ad3cba1ad525c4195
aasumar123 <aas24@rice.edu>
2024-07-06 02:07:47 -0500

    deploy site

---
+++ docs/_next/static/webpack/pages/people/court.e72415b9ff2f1fc17a37.hot-update.js
@@ -0,0 +1,15 @@
+webpackHotUpdate_N_E("pages/people/court",{
+
+/***/ "./components/people/court/court.json":
+/*!********************************************!*\
+  !*** ./components/people/court/court.json ***!
+  \********************************************/
+/*! exports provided: justices, default */
+/***/ (function(module) {
+
+module.exports = JSON.parse("{\"justices\":[{\"position\":\"Chief Justice\",\"name\":\"Kile Stenoien\",\"email\":\"mcmurtrycj@gmail.com\",\"phone\":\"832-416-0948\",\"room\":\"480\",\"bio\":\"yeeeeet\"},{\"position\":\"Chief Justice\",\"name\":\"Jackson Harvey\",\"email\":\"mcmurtrycj@gmail.com\",\"phone\":\"832-370-6826\",\"room\":\"486\"},{\"position\":\"Associate Justice\",\"name\":\"Vidal Saenz\",\"email\":\"mcmurtrycj@gmail.com\",\"room\":\"334\"},{\"position\":\"Associate Justice\",\"name\":\"Jimin Lee\",\"phone\":\"516-455-2667\",\"room\":\"446\"},{\"position\":\"Associate Justice\",\"name\":\"Pedro Ribeiro\",\"phone\":\"561-562-2507\",\"room\":\"290\"},{\"position\":\"Associate Justice\",\"name\":\"Shreyan Das\",\"phone\":\"732-688-8293\",\"room\":\"549\"},{\"position\":\"Associate Justice\",\"name\":\"Sabrina Sun\",\"phone\":\"626-823-1115\",\"room\":\"502\"},{\"position\":\"Associate Justice\",\"name\":\"Nick He\",\"phone\":\"626-222-6363\",\"room\":\"OC\"}]}");
+
+/***/ })
+
+})
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzdGF0aWMvd2VicGFjay9wYWdlcy9wZW9wbGUvY291cnQuZTcyNDE1YjlmZjJmMWZjMTdhMzcuaG90LXVwZGF0ZS5qcyIsInNvdXJjZVJvb3QiOiIifQ==

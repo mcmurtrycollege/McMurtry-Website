5cb49e0ed5cfea43e01f41a8616c4f85d1b9c907
aasumar123 <aas24@rice.edu>
2024-07-06 02:28:16 -0500

    deploy site

---
+++ docs/_next/static/webpack/pages/index.072b1f506f22cc97dd71.hot-update.js
@@ -0,0 +1,15 @@
+webpackHotUpdate_N_E("pages/index",{
+
+/***/ "./components/navbar/navbar.json":
+/*!***************************************!*\
+  !*** ./components/navbar/navbar.json ***!
+  \***************************************/
+/*! exports provided: navbar_headers, default */
+/***/ (function(module) {
+
+module.exports = JSON.parse("{\"navbar_headers\":[{\"name\":\"About\",\"subheaders\":[{\"name\":\"About\",\"to\":\"/about\"},{\"name\":\"O-Week\",\"to\":\"/oweek\"}]},{\"name\":\"About\",\"subheaders\":[{\"name\":\"About\",\"to\":\"/about\"},{\"name\":\"O-Week\",\"to\":\"/oweek\"}]},{\"name\":\"Events\",\"subheaders\":[{\"name\":\"Calendar\",\"to\":\"/events/calendar\"},{\"name\":\"Budget/Expenses\",\"to\":\"/events/budget\"}]},{\"name\":\"People\",\"subheaders\":[{\"name\":\"McTeam\",\"to\":\"/people/mcteam\"},{\"name\":\"McMinistry\",\"to\":\"/people/mcministry\"},{\"name\":\"McCourt\",\"to\":\"/people/court\"},{\"name\":\"Committees\",\"to\":\"/people/committees\"},{\"name\":\"Affinity Groups\",\"to\":\"/people/mcmurtryaffinitygroups\"},{\"name\":\"Associates\",\"to\":\"/people/associates\"},{\"name\":\"Peer Academic Advisors\",\"to\":\"/people/PAAs\"},{\"name\":\"Academic Fellows\",\"to\":\"/people/academicfellows\"},{\"name\":\"RHAs\",\"to\":\"/people/RHAs\"},{\"name\":\"Head Caregivers\",\"to\":\"/people/headcaregivers\"},{\"name\":\"SMR\",\"to\":\"/people/SMR\"},{\"name\":\"Divisional Advisors\",\"to\":\"/people/divisionaladvisors\"}]},{\"name\":\"Resources\",\"subheaders\":[{\"name\":\"Room Reservations\",\"to\":\"/resources/roomreservations\"},{\"name\":\"Financial Inclusivity\",\"to\":\"/about/financialinclusivity\"},{\"name\":\"MIS\",\"to\":\"/resources/mis\"},{\"name\":\"Documents\",\"to\":\"/resources/documents\"},{\"name\":\"Diversity Resources\",\"to\":\"/resources/diversityResources\"},{\"name\":\"Work Orders\",\"to\":\"/resources/workorders\"},{\"name\":\"Initiative Requests\",\"to\":\"/resources/initiativerequests\"},{\"name\":\"Feedback Form\",\"to\":\"/resources/feedbackform\"}]}]}");
+
+/***/ })
+
+})
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzdGF0aWMvd2VicGFjay9wYWdlcy9pbmRleC4wNzJiMWY1MDZmMjJjYzk3ZGQ3MS5ob3QtdXBkYXRlLmpzIiwic291cmNlUm9vdCI6IiJ9

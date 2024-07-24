671b11d00eea89493235a35d2cac75ca06328191
fnemati <fn11@rice.edu>
2024-07-23 23:31:36 -0500

    oweek book and video

---
+++ docs/_next/static/webpack/styles.9c6a567a5b3b0306eb78.hot-update 2.js
@@ -0,0 +1,27 @@
+webpackHotUpdate_N_E("styles",{
+
+/***/ "./components/general/footer/footer.css":
+false,
+
+/***/ "./components/navbar/NavbarDesktop.css":
+false,
+
+/***/ "./components/navbar/navbarMobile.css":
+false,
+
+/***/ "./components/splashpage/intropage.css":
+false,
+
+/***/ 1:
+false,
+
+/***/ 2:
+false,
+
+/***/ 3:
+false,
+
+/***/ 4:
+false
+
+})

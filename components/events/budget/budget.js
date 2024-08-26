e06bc0c5eecbc9842aadfd7a180244daa432c60e
aasumar123 <aas24@rice.edu>
2024-08-26 12:04:52 -0500

    switching out new expense form link

--- components/events/budget/budget.js
+++ components/events/budget/budget.js
@@ -7,13 +7,13 @@ export default class Budget extends React.Component {
     render() {
         return (
             <div className='budget-sheet-page'>
-                <Title width={400} title="Budget Sheet" />
+                <Title width={400} title="Expense Form" />
+                <Title width={400} title="Budget Sheet and Expense Form" />
                 <Box width={[0.8, 0.25]} ml='auto' mr='auto'>
                     <p><a href="https://docs.google.com/spreadsheets/d/17f2MzrfQ9wlrDQBXBXbiBjCPLqaBW2iupWF5ocEx5T0/edit?usp=sharing">Link to the most recent McMurtry Budget Sheet</a></p>
                 </Box>
-                <Title width={400} title="McMurtry Expenditure Form" />
                 <Box width={[0.8, 0.25]} ml='auto' mr='auto'>
-                    <p><a href="https://docs.google.com/forms/d/e/1FAIpQLScudpfQhAaGSpMu4t9Me4o_Zu767Jmqy1bG2CaXgjhpPwQ7Ag/viewform">Link to the McMurtry Expenditure Form</a></p>
+                    <p><a href="https://docs.google.com/forms/d/e/1FAIpQLSd4LCmTDO2fXjyeM3H0dJ82IY1RdyasAqhGHl2Nuouqj0gAFg/viewform">Link to the McMurtry Expense Form</a></p>
                 </Box>
             </div>
         )

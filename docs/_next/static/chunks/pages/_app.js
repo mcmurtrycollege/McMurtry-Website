42b02aa4ce64293d7d52fc0575bc1ed07ec8a1e1
aasumar123 <aas24@rice.edu>
2024-08-26 12:05:36 -0500

    deploy site

--- docs/_next/static/chunks/pages/_app.js
+++ docs/_next/static/chunks/pages/_app.js
@@ -6648,7 +6648,7 @@ if (false) {} else {
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! next-client-pages-loader?page=%2F_app&absolutePagePath=next%2Fdist%2Fpages%2F_app! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F_app&absolutePagePath=next%2Fdist%2Fpages%2F_app!./");
-module.exports = __webpack_require__(/*! C:\Users\Owner\Documents\McMurtry-Website\node_modules\next\dist\client\router.js */"./node_modules/next/dist/client/router.js");
+module.exports = __webpack_require__(/*! C:\Murt Website\McMurtry-Website\node_modules\next\dist\client\router.js */"./node_modules/next/dist/client/router.js");
 
 
 /***/ })

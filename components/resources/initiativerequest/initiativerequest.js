1089ff1b7f499342bf01026c1520739873a8a043
aasumar123 <aas24@rice.edu>
2024-08-26 23:01:22 -0500

    switching McFUNd request form and changing page name

--- components/resources/initiativerequest/initiativerequest.js
+++ components/resources/initiativerequest/initiativerequest.js
@@ -5,12 +5,12 @@ import './initiativerequest.css';
 
 const InitiativeRequest = () => (
     <div className='initiative-request-page'>
-        <Title width={340} title="Initiative Request" />
+        <Title width={340} title="McFUNd" />
         <div className='wide-initiative-request-form'>
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeIxEkrO--c-dXURvAl1ogK3LDVLZiQbPPwEPEGI4XnOimzew/viewform" width="700" height="2000" frameBorder="0" scrolling="yes" type="text/javascript" title="Initiatives Request"></iframe>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdHfWng8IW37QGSMz9lObCNHCRC1ifZjGxola_7udR-4A-f-Q/viewform?usp=sf_link" width="700" height="2000" frameBorder="0" scrolling="yes" type="text/javascript" title="McFUNd Request"></iframe>
         </div>
         <div className='narrow-initiative-request-form'>
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeIxEkrO--c-dXURvAl1ogK3LDVLZiQbPPwEPEGI4XnOimzew/viewform" width="360" height="2000" frameBorder="0" scrolling="yes" type="text/javascript" title="Initiatives Request"></iframe>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdHfWng8IW37QGSMz9lObCNHCRC1ifZjGxola_7udR-4A-f-Q/viewform?usp=sf_link" width="360" height="2000" frameBorder="0" scrolling="yes" type="text/javascript" title="McFUNd Request"></iframe>
         </div>
     </div>
 )

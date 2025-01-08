b46cf53a7c5e45a56188dff8e22ac4514563c540
Stone Li <slsd57103@hotmail.com>
2025-01-08 11:39:04 -0500

    deploy site: updated head caregivers

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f31df7b5ff853935d9f3";
+/******/ 	var hotCurrentHash = "37c3b46a4aa183d5ebcc";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;

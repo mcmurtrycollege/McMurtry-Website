e6ee66b4deb16bbbc5d3d60bbf0b8e3ddaaf571c
Daniel <dgalabay012@gmail.com>
2025-07-10 23:10:17 -0400

    Added OWeek 2025 Book

--- components/oweek/book/book.js
+++ components/oweek/book/book.js
@@ -3,15 +3,14 @@ import '../oweek.css';
 const Book = () => (
     <div className='oweek-page'>
         <h1 className='oweek-title'>The O-Week Book</h1>
-        <a href='../../../static/oweek/oweekbook-2024.pdf' target='_blank' rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
+        <a href='../../../static/oweek/oweekbook-2025.pdf' target='_blank' rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
             <div className='oweek-book-download'>
                 {<img src="https://icon.now.sh/file_download/DCDFE5/60" alt="" />}
                 {<p>The O-Week Book</p>}
-                {<p>is coming soon...</p>}
             </div>
         </a>
         <div className='oweek-book'>
-            <embed src='../../../static/oweek/oweekbook-2024.pdf' type="application/pdf" width="600" height="900px" page="7" scrollbar="1" toolbar="1" />
+            <embed src='../../../static/oweek/oweekbook-2025.pdf' type="application/pdf" width="600" height="900px" page="7" scrollbar="1" toolbar="1" />
         </div>
     </div>
 )

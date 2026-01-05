03788735df8d92b6ab408b03d210e988c91abd2c
Antony Saleh <antony.saleh2017@gmail.com>
2026-01-06 01:12:50 +0200

    Fix CSS paths for GitHub Pages subdirectory deployment

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 // next.config.js
 const withCSS = require('@zeit/next-css')
-module.exports = withCSS()
+
+module.exports = withCSS({
+  // Use basePath for GitHub Pages deployment at subdirectory
+  basePath: '/McMurtry-Website',
+  assetPrefix: '/McMurtry-Website',
+})
